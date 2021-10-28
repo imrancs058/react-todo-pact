9a0733ebe42d7f8d537e21d0ffe076909ce3ddc0
imrancs058 <imrancs058@yahoo.com>
2021-10-28 15:11:15 +0500

    Unit/UI tests for todo that fails

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
+import { mount } from 'enzyme';
+var wrapper;
+beforeEach(()=>{
+  wrapper=mount(<App/>);
+})
+describe("Test Cases for todo module",()=>{
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  test('TODO input field, Add button,Display TODO list element exists', () => {
+    expect(wrapper.find("#add-todo-txt").length).toEqual(1);
+    expect(wrapper.find("#add-todo-btn").length).toEqual(1);
+    expect(wrapper.find("#add-todo-list").length).toEqual(1);
+  });
+  test("Render button named Add",()=>{
+    expect(wrapper.find("#add-todo-btn").text()).toBe("Add")
+  })
+})
